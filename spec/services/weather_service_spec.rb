8e442fadd54598254660f9a6035deb5e31caf790
Chris Romero <chrisleeromero1@gmail.com>
2022-06-11 16:58:04 -0600

    weather service provides forecast data

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Weather Service' do
-  xit 'gets weather data based on longitude and latitude' do
-
+  it 'gets weather data based on longitude and latitude' do
+    location = 'Denver,CO'
+    forecast = WeatherService.get_forecast(location)
+    expect(forecast).to be_a(Hash)
   end
 end
