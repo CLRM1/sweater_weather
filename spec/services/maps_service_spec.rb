4893ef0c825907ff21649b14d6f642f153903f30
Chris Romero <chrisleeromero1@gmail.com>
2022-06-11 16:16:06 -0600

    maps service returns lat and long coordinates for a city, state

--- spec/services/maps_service_spec.rb
+++ spec/services/maps_service_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Maps Service' do
-  xit 'returns the longitude and latitude for a given city' do
+  it 'returns the longitude and latitude for a given city' do
+    location = 'Denver,CO'
+    coordinates = MapsService.get_coordinates(location)
+    lat = coordinates[:results][0][:locations][0][:latLng][:lat]
+    long = coordinates[:results][0][:locations][0][:latLng][:lng]
 
+    expect(lat).to eq(39.738453)
+    expect(long).to eq(-104.984853)
   end
 end
