ae816c6fc2818fe3215c3d6dc65beca2c67ed0f7
Chris Romero <chrisleeromero1@gmail.com>
2022-06-12 01:47:26 -0600

    refactor: remove state from pexel search

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    image_data = ImageFacade.get_image(params[:location])
+    city = params[:location].split(',').first
+    image_data = ImageFacade.get_image(city)
     render json: ImageSerializer.format_image_data(image_data)
   end
 end
