360d5df4850fe208f528b17c75ca439a47bb64df
Chris Romero <chrisleeromero1@gmail.com>
2022-06-13 10:10:25 -0600

    wip: continue making poro and serializer for book response

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,7 +1,37 @@
 class Book
-  attr_reader :data
+  attr_reader :books,
+              :isbn_0,
+              :isbn_1,
+              :isbn_2,
+              :isbn_3,
+              :isbn_4,
+              :title_0,
+              :title_1,
+              :title_2,
+              :title_3,
+              :title_4,
+              :publisher_1,
+              :publisher_2,
+              :publisher_3,
+              :publisher_4,
+
   def initialize(book_data)
-    @argument = argument
+    @total_books_found = book_data[:numFound]
+    @books = book_data[:docs][0..4]
+    @isbn_0 = @books[0][:isbn]
+    @isbn_1= @books[1][:isbn]
+    @isbn_2 = @books[2][:isbn]
+    @isbn_3 = @books[3][:isbn]
+    @isbn_4 = @books[4][:isbn]
+    @title_0 = @books[0][:title]
+    @title_1 = @books[1][:title]
+    @title_2 = @books[2][:title]
+    @title_3 = @books[3][:title]
+    @title_4 = @books[4][:title]
+    @publisher_1 = @books[1][:publisher]
+    @publisher_2 = @books[2][:publisher]
+    @publisher_3 = @books[3][:publisher]
+    @publisher_4 = @books[4][:publisher]
   end
 
 end
