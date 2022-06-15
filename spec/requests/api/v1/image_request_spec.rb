f56b04a0da77756a81a2e7674ba3f9e439be295f
Chris Romero <chrisleeromero1@gmail.com>
2022-06-14 22:38:51 -0600

    test: image response should not contain extra fields from pexel API

--- spec/requests/api/v1/image_request_spec.rb
+++ spec/requests/api/v1/image_request_spec.rb
@@ -15,4 +15,16 @@ RSpec.describe 'Image API' do
     expect(body[:data][:attributes][:image][:photographer]).to be_a(String)
     expect(body[:data][:attributes][:image][:photographer_url]).to be_a(String)
   end
+
+  it 'does not return other data included in the response from the image API provider' do
+    get '/api/v1/backgrounds?location=denver,co'
+
+    body = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(body[:data]).to_not have_key(:page)
+    expect(body[:data]).to_not have_key(:per_page)
+    expect(body[:data]).to_not have_key(:photo)
+    expect(body[:data]).to_not have_key(:width)
+    expect(body[:data]).to_not have_key(:next_page)
+  end
 end
